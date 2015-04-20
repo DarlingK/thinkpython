064c5216ed3a5ce6dde8e69efda6e4dc2d762c97
DarlingK <yourdarlingv@126.com>
2015-04-20 21:37:59 +0800

    Ch4 案例研究：接口设计
    4.1 乌龟的世界

---
+++ mypolygon.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 21:34:24 2015
+
+@author: Flappy-Man
+"""
+
+from swampy.TurtleWorld import *
+
+world=TurtleWorld()
+bob=Turtle()
+print bob
+
+wait_for_user()
+
+
